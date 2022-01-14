03c52aaf4740803d8672029f3751aa9f37019038
Erhan <65120828+erhnk@users.noreply.github.com>
2022-01-14 13:03:07 +0300

    Create Java-ile-Taksimetre-Programı.java

---
+++ Java-ile-Taksimetre-Programı.java
@@ -0,0 +1,30 @@
+//Java-ile-Taksimetre-Programı
+
+public class MyClass {
+    public static void main(String args[]) {
+     
+     double kmBasinaUcret, odenecekTutar, kilometre, MesafeyeGoreUcret,HesaplananTutar;
+     int acilisUcreti;
+     
+     Scanner input = new Scanner(System.in);
+     
+     kilometre = input.nextDouble();
+     
+     acilisUcreti = 10;
+     
+     kmBasinaUcret = 2.20;
+     
+     MesafeyeGoreUcret = kmBasinaUcret * kilometre;
+     
+     HesaplananTutar = MesafeyeGoreUcret + acilisUcreti;
+     
+     odenecekTutar = (HesaplananTutar >= 20) ? HesaplananTutar : 20;
+     
+     System.out.println("Gidilen kilometre : " + kilometre);
+     System.out.println("Acılıs ucreti : " + acilisUcreti);
+     System.out.println("Mesafeye göre ucret : " + MesafeyeGoreUcret);
+     System.out.println("Hesaplanan Tutar : " + HesaplananTutar);
+     System.out.println("Odenecek Tutar : " + odenecekTutar);
+    
+    }
+}
